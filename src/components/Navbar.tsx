6fff1bc1dad7318f452b51078d76c095cad2fb29
Floressek <szymon.florek@student.wat.edu.pl>
2025-03-24 21:14:26 +0100

    Refactor Navbar to include a styled header with a logo.
    Replaced the previous session-based rendering with a fixed header displaying a clickable "Quizzy" logo using Next.js Link. Prepared the component structure for a more interactive and visually appealing navigation bar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import {getAuthSession} from "@/lib/nextAuth";
 
 
@@ -6,11 +7,23 @@ type Props = {};
 
 // Runs once on the server
 const Navbar = async (props: Props) => {
-    const session = await getAuthSession();
-    if(session?.user) {
-        return <pre>{JSON.stringify(session.user, null, 2)}</pre>
-    }
-    return <div>Not signed in</div>;
+    // const session = await getAuthSession();
+    // if(session?.user) {
+    //     return <pre>{JSON.stringify(session.user, null, 2)}</pre>
+    // }
+    // return <div>Not signed in</div>;
+    return (
+        <div className={"fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2"}>
+            <div className={"flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl"}>
+                {/* Logo */}
+                <Link href={"/"} className={"flex items-center gap-2"}>
+                    <p className={"rounded-lg border-2 border-b-4 border-r-4 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white"}>
+                        Quizzy
+                    </p>
+                </Link>
+            </div>
+        </div>
+    )
 };
 
 export default Navbar;
