1f7e1ad25d51ad890b955f2ac8ccf17451640237
Floressek <szymon.florek@student.wat.edu.pl>
2025-03-25 01:36:46 +0100

    Add and enhance user avatar and account navigation components
    Introduce a reusable `Avatar` component with subcomponents for image and fallback states. Extend `UserAccountNav` with improved user UI, including email display, better navigation structure, and a styled sign-out option. Placeholder added for future navigation links.

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,7 +1,18 @@
+"use client";
 import React from "react";
 import {User} from "next-auth";
-import {DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 import Image from "next/image";
+import Link from "next/link";
+import {signOut} from "next-auth/react";
+import {logger} from "@/lib/client-logger";
+import {LogOut} from "lucide-react";
 
 type Props = {
     user: Pick<User, "name" | "image" | "email">;
@@ -12,16 +23,42 @@ const UserAccountNav = ({user}: Props) => {
         <DropdownMenu>
             <DropdownMenuTrigger
                 className={"flex items-center gap-2 rounded-md border-2 border-black px-2 py-1 text-xl font-bold transition-all hover:-translate-y-[2px] md:block dark:border-white"}>
-                <Image src={user.image || ""} alt={user.name || ""} width={20} height={20} className={"h-8 w-8 rounded-full"}/>
+                <Image src={user.image || ""} alt={user.name || ""} width={20} height={20}
+                       className={"h-8 w-8 rounded-full items-center "}/>
                 <p className={"hidden md:block"}>{user.name}</p>
             </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuItem>
-                    <a href={"/profile"}>Profile</a>
+            {/*Email + Name -- SECTION 1*/}
+            <DropdownMenuContent className={"bg-white dark:bg-gray-950"} align={"end"}>
+                <div className={"flex items-center justify-start gap-2 p-2"}>
+                    <div className={"flex flex-col space-y-1 leading-none"}>
+                        {user.name && <p className={"font-medium"}>{user.name}</p>}
+                        {user.email && (
+                            <p className={"w-[200px] truncate text-sm text-zinc-700"}>
+                                {user.email}
+                            </p>
+                        )}
+                    </div>
+                </div>
+
+                <DropdownMenuSeparator/>
+                {/*CHANGE IT -- SECTION 2*/}
+                <DropdownMenuItem asChild>
+                    <Link href={'/'}>Placeholder</Link>
                 </DropdownMenuItem>
-                <DropdownMenuItem>
-                    <a href={"/api/auth/signout"}>Sign out</a>
+
+                <DropdownMenuSeparator/>
+                {/*SIGN OUT -- SECTION 3*/}
+                <DropdownMenuItem onClick={(e)=> { // Onclick event == "use client"
+                    e.preventDefault();
+                    signOut().catch((err) => {
+                        logger.error("Error signing out:", err);
+                    });
+                }}
+                className={"text-red-500 cursor-pointer"}>
+                    Sign out
+                    <LogOut className={"text-red-500 ml-auto h-4 w-4"}/>
                 </DropdownMenuItem>
+
             </DropdownMenuContent>
         </DropdownMenu>
     )
---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {User} from "next-auth";
+
+type Props = {
+    user: Pick<User, "name" |  "image">;
+}
+
+// Reusable button component
+const UserAvatar = (props: Props) => {
+    return (
+    )
+}
+
+export default UserAvatar
+
