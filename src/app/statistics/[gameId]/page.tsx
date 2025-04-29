bc8f06583ddb4d963f2f85d72cfe2db6b18e7770
Floressek <szymon.florek@student.wat.edu.pl>
2025-04-29 02:15:36 +0200

    Refactor statistics page to handle game data retrieval
    Integrated database query to fetch game details based on gameId and added authentication checks with redirects for unauthenticated users and invalid game IDs. Enhanced UI with a dashboard link and structured content layout.

--- src/app/statistics/[gameId]/page.tsx
+++ src/app/statistics/[gameId]/page.tsx
@@ -1,25 +1,54 @@
 import React from "react";
 import {getAuthSession} from "@/lib/nextAuth";
 import {redirect} from "next/navigation";
-import QuizCreation from "@/components/QuizCreation";
+import { prisma } from "@/lib/db";
+import { LucideLayoutDashboard } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
-type Props = {}
+type Props = {
+    params: Promise<{
+        gameId: string;
+    }>
+}
 
 export const metadata = {
     title: "Statistics | Quizzy",
     description: "Statistics Page",
 }
 
-const StatisticsPage = async (props: Props) => {
-    // const session = await getAuthSession();
-    // if (!session?.user) {
-    //     // User is not logged in
-    //     redirect("/?error=auth_required");
-    // }
+const StatisticsPage = async ({params}: Props) => {
+    const {gameId} = await params; // do not remove, this is some gimmicky next.js shit
+    const session = await getAuthSession();
+    if (!session?.user) {
+        // User is not logged in
+        redirect("/?error=auth_required");
+    }
+    const game = await prisma.game.findUnique({
+        where: {id: gameId}
+    });
+
+    if (!game) {
+        // Game not found
+        redirect("/quiz?error=game_not_found");
+    }
+
     return (
-        <div>Hello</div>
+        <>
+            <div className="p-8 mx-auto max-2-7xl">
+                <div className="flex items-center justify-between space-y-2">
+                    <h2 className="text-3xl font-bold tracking-tight">Statistics</h2>
+                    <div className="flex items-center space-x-2">
+                        <Link href="/dashboard" className={buttonVariants()}>
+                            Back to Dashboard
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
 export default StatisticsPage;
+
 
