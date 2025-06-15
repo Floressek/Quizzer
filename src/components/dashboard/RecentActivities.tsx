93facd44ab619ed5fc27dae980b3f23fee982106
iskow <izabela.skowron@student.wat.edu.pl>
2025-06-16 01:45:11 +0200

    History Page

--- src/components/dashboard/RecentActivities.tsx
+++ src/components/dashboard/RecentActivities.tsx
@@ -1,26 +1,31 @@
 import React from "react";
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import HistoryComponent from "@/components/HistoryComponent";
+import { getAuthSession } from "@/lib/nextAuth";
 
 type Props = {}
 
-const RecentActivities = (props: Props) => {
-    return (
-        <Card className="col-span-4 lg:col-span-3">
-            <CardHeader>
-                <CardTitle className="text-2xl font-bold">
-                    Recent Activities
-                </CardTitle>
-                <CardDescription>
-                    {/*TODO: change to the value from db when we will have actual quizzes / metadata regarding them*/}
-                    You have played a total of X number of quizzes.
-                </CardDescription>
-            </CardHeader>
-            <CardContent className="max-h-[580px] overflow-scroll">
-                History of your played games will be displayed here.
-            </CardContent>
-        </Card>
-    )
-}
+const RecentActivities = async (props: Props) => {
+    const session = await getAuthSession();
+    if (!session?.user) {
+        return redirect("/");
+        }
+        return (
+            <Card className="col-span-4 lg:col-span-3">
+                <CardHeader>
+                    <CardTitle className="text-2xl font-bold">
+                        Recent Activities
+                    </CardTitle>
+                    <CardDescription>
+                        {/*TODO: change to the value from db when we will have actual quizzes / metadata regarding them*/}
+                        You have played a total of X number of quizzes.
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="max-h-[580px] overflow-scroll">
+                    <HistoryComponent limit={10} userId={session.user.id}/>
+                </CardContent>
+            </Card>
+        )
+    }
 
 export default RecentActivities;
-
